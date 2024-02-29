e6c3a7f26d18ca9c5b81e7b6b59279f9d59e8ebf
moscuet <mos.cuet@gmail.com>
2024-02-29 21:42:49 +0200

    initial cleaning template

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'air-bnb':'#ff385c',
+        'air-bnb-dark':'#d50027'
+      },
+      screens: {
+        'xl': '1440px',
       },
     },
+    plugins: [],
   },
-  plugins: [],
 };
 export default config;
