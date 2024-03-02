d14ed41eda2bdfab6c8fa8180277d017f1c767c1
moscuet <mos.cuet@gmail.com>
2024-03-02 12:01:11 +0200

    add navmenu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import "../styles/navbar.css";
 import SerachFilter from "./SerachFilter";
+import UserNav from "./NavMenu";
 
 export default function Navbar() {
   return (
@@ -32,16 +33,27 @@ export default function Navbar() {
             </div>
           </>
 
-          <div className="hidden xl:block w-[400px] h-[48px] px-[24px] border mx-auto"></div>
+          <div className="hidden xl:block w-[400px] h-[48px] px-[24px] border mx-auto">
+            stay1
+          </div>
+          <div className="mx-[40px] flex items-center sm:hidden">
+            <SerachFilter />
+          </div>
 
-          <div>add property</div>
+          <div>
+            add property
+            <UserNav/>
+          </div>
         </div>
       </div>
-      <div className="xl:hidden flex justify-center">
-        <div className="w-[400px] h-[48px] my-2 px-[24px] border">Stay</div>
+
+      <div className="hidden sm:flex md:flex lg:flex xl:hidden justify-center">
+        <div className="w-[400px] h-[48px] my-2 px-[24px] border">Stay2</div>
       </div>
 
-      <SerachFilter />
+      <div className="mx-[40px] hidden sm:block">
+        <SerachFilter />
+      </div>
     </div>
   );
 }
