99fd5387eb5fd7c0bd9aebdfb903296098cf93c3
moscuet <mos.cuet@gmail.com>
2024-03-04 02:56:33 +0200

    update laypout with Main

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,9 @@ import PropertieList from "./components/properties/PropertieList";
 
 export default function Home() {
   return (
-    <main className="px-5 mt-[92px] sm:mt-[254px] lg:mt-[242px] xl:mt-[178px]  sm:px-[40px] 3xl:px-[80px] max-w-[1680px]">
       <div>
         <Catagories />
         <PropertieList />
       </div>
-    </main>
   );
 }
